ab98260ef8c625d215b5028a96176eabfa325a78
Gabe Kälin <gabe@dehei.me>
2024-12-02 05:15:46 +0000

    chore: add integration

---
+++ custom_components/grill_eye/const.py
@@ -0,0 +1,8 @@
+"""Constants for grill_eye."""
+
+from logging import Logger, getLogger
+
+LOGGER: Logger = getLogger(__package__)
+
+DOMAIN = "grill_eye"
+ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
